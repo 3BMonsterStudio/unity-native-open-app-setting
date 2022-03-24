1966d6977a6b6798d09e2e91b1bd79deb14e2efb
quanpna-monster <quanpna@monsterstudio.io>
2022-03-24 10:24:23 +0700

    Check unity editor

--- Runtime/OpenAppSettingsSDK.cs
+++ Runtime/OpenAppSettingsSDK.cs
@@ -7,7 +7,7 @@ public class OpenAppSettingsSDK
 {
     public static void OpenAppSettingDetailPopup()
     {
-#if UNITY_ANDROID
+#if UNITY_ANDROID && !UNITY_EDITOR
         OpenAppSettingsNativeAndroid.OpenSettings();
 #endif
 #if UNITY_IOS && !UNITY_EDITOR
