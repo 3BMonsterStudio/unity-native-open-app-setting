33ad30740945267844d8de7fc97eac9adfd44775
quanpna-monster <quanpna@monsterstudio.io>
2022-03-24 10:18:54 +0700

    Update path

--- Runtime/OpenAppSettingsSDK.cs
+++ Runtime/OpenAppSettingsSDK.cs
@@ -1,10 +1,13 @@
 using AppSettingsSDK;
-
+/// <summary>
+/// This class only work in mobile device
+/// Remember to check is UNITY_EDITOR when call this function
+/// </summary>
 public class OpenAppSettingsSDK
 {
     public static void OpenAppSettingDetailPopup()
     {
-#if UNITY_ANDROID && !UNITY_EDITOR
+#if UNITY_ANDROID
         OpenAppSettingsNativeAndroid.OpenSettings();
 #endif
 #if UNITY_IOS && !UNITY_EDITOR
